914e011fa0a2d0e230efcb78bbbb31de55ab6fe4
ewt45 <79033456+ewt45@users.noreply.github.com>
2025-02-21 15:15:41 +0800

    升级主题版本及部分依赖

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -11,15 +11,13 @@ export default defineUserConfig({
   // description: '我的博客',
   pagePatterns: ['**/*.md', '!**/README.md', '!.vuepress', '!node_modules'], //排除readme
   theme: recoTheme({
-    style: '@vuepress-reco/style-default',
     // reco2 不知道怎么用的左侧栏(成功了，想在哪个页面显示左侧栏就添加一项，字符串为其父目录，数组里最好用对象形式，否则文章frontmatter里的title会默认变成左侧栏的文字。自定义显示文字用原生的sidebar，text自定义，link写相对路径，因为我的index.md 的浏览路径就是父路径，所以没办法加了个./。然后运行发现报错children.map的children为undefined，原来就算没children也要定义一个空的数组
     //还有就是原生的没有样式，reco的写法才有样式)
     series: {
       '/blogs/exagearInitContainer': [
         { text: '主篇', link: './', children: [] }
         , { text: '旧方法', link: 'detailed', children: [] }
-      ]
-      ,
+      ],
 
       // '/blogs/exagearInitContainer':['./','detailed']
       // '/blogs/exagearInitContainer':[
@@ -35,7 +33,7 @@ export default defineUserConfig({
     // },
     // type: 'blog',
     //reco1 右侧栏，当前文章的章节
-    subSidebar: 'auto',
+    // subSidebar: 'auto',
     //顶部导航栏
     // navbar: [
     //   { text: '首页', link: '/', icon: 'reco-home' },
@@ -45,7 +43,6 @@ export default defineUserConfig({
     //     ]
     //   },
     // ],
-    componentsDir: './docs/.vuepress/components',
 
   }),
 
