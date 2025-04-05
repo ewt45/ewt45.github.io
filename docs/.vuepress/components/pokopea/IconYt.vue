8c1a383aeeb40c0fca0ccd5ab0fa5cce041624dc
ewt45 <79033456+ewt45@users.noreply.github.com>
2025-04-05 16:47:39 +0800

    狸豆页面：图标深色

--- docs/.vuepress/components/pokopea/IconYt.vue
+++ docs/.vuepress/components/pokopea/IconYt.vue
@@ -1,27 +1,23 @@
 <template>
-    <svg version="1.1" id="svg1" width="161.98" height="113.38666" viewBox="0 0 161.97999 113.38666"
-        xmlns="http://www.w3.org/2000/svg" xmlns:svg="http://www.w3.org/2000/svg">
-        <defs id="defs1">
-            <clipPath clipPathUnits="userSpaceOnUse" id="clipPath2">
-                <path d="M 0,255.118 H 550.024 V 0 H 0 Z" transform="translate(-203.4961,-156.7999)" id="path2" />
-            </clipPath>
-            <clipPath clipPathUnits="userSpaceOnUse" id="clipPath4">
-                <path d="M 0,255.118 H 550.024 V 0 H 0 Z" transform="translate(-133.14351,-109.336)" id="path4" />
-            </clipPath>
-        </defs>
-        <g id="layer-MC0" transform="translate(-112.73347,-113.38547)">
-            <path id="path1"
+    <svg width="161.98" height="113.38666" viewBox="0 0 161.97999 113.38666" xmlns="http://www.w3.org/2000/svg">
+        <g transform="translate(-112.73347,-113.38547)">
+            <path :fill="isDark ? '#bb0000' : '#ff0000'"
                 d="m 0,0 c -1.397,5.227 -5.514,9.343 -10.741,10.74 -9.474,2.539 -47.463,2.539 -47.463,2.539 0,0 -37.989,0 -47.463,-2.539 -5.227,-1.397 -9.344,-5.513 -10.741,-10.74 -2.538,-9.474 -2.538,-29.241 -2.538,-29.241 0,0 0,-19.767 2.538,-29.24 1.397,-5.227 5.514,-9.344 10.741,-10.741 9.474,-2.539 47.463,-2.539 47.463,-2.539 0,0 37.989,0 47.463,2.539 5.227,1.397 9.344,5.514 10.741,10.741 2.539,9.473 2.539,29.24 2.539,29.24 0,0 0,19.767 -2.539,29.241"
-                style="fill:#ff0000;fill-opacity:1;fill-rule:nonzero;stroke:none"
-                transform="matrix(1.3333333,0,0,-1.3333333,271.32813,131.0908)" clip-path="url(#clipPath2)" />
-            <path id="path3" d="M 0,0 31.562,18.223 0,36.446 Z"
-                style="fill:#ffffff;fill-opacity:1;fill-rule:nonzero;stroke:none"
-                transform="matrix(1.3333333,0,0,-1.3333333,177.52467,194.376)" clip-path="url(#clipPath4)" />
+                transform="matrix(1.3333333,0,0,-1.3333333,271.32813,131.0908)" />
+            <path :fill="isDark ? '#bbbbbb' : '#ffffff'" d="M 0,0 31.562,18.223 0,36.446 Z"
+                transform="matrix(1.3333333,0,0,-1.3333333,177.52467,194.376)" />
         </g>
     </svg>
 </template>
+
+<script setup lang="ts">
+import { useDark } from '@vueuse/core'
+const isDark = useDark()
+</script>
+
 <style scoped>
 svg {
     padding: 2px;
+    color: #bbbbbb;
 }
 </style>
